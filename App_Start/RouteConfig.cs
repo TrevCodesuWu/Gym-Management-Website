a3079c56fcfc44707fb525d17c55e74787a4a1b2
Trevlin Naicker <Trevlinnaicker22@gmail.com>
2023-03-21 15:46:34 +0200

    Gym-Home-Page

--- App_Start/RouteConfig.cs
+++ App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace Gym_Management_Website
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Gym", action = "GymHome", id = UrlParameter.Optional }
             );
         }
     }
---
+++ Controllers/GymController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Gym_Management_Website.Controllers
+{
+    public class GymController : Controller
+    {
+        // GET: Gym
+        public ActionResult GymHome()
+        {
+            return View();
+        }
+
+    }
+}
